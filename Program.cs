dfb354d95e187a83b633a31bbaa0ceea158be611
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-05 23:40:59 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+namespace ConsoleEx000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Olá, Mundo!");
+            ReadKey();
+        }
+    }
+}
